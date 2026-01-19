77615def25d2906de00786d19d39e885254d0979
COMRADE040 <rafiulislam040@gmail.com>
2026-01-19 17:36:36 +0600

    build

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,10 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  theme: {
-    extend: {
-      colors: {
-        background: 'var(--color-background)',
-        foreground: 'var(--color-foreground)',
-      },
-      fontFamily: {
-        sans: 'var(--font-sans)',
-        mono: 'var(--font-mono)',
-      },
-    },
-  },
-  plugins: [],
-}
+};
 
 
